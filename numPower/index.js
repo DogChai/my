12ea27b0d27333c883c3c05d2480576ae99f3899
dogchai <767733103@qq.com>
2018-06-24 21:19:04 +0800

    结构

---
+++ numPower/index.js
@@ -0,0 +1,29 @@
+var runPower = (function() {
+
+    //初始化参数
+    function  _runPower() {
+        this.num = num;
+    }
+
+    //原型方法    
+    _runPower.prototype = {
+        init: function() {
+
+        },
+
+        render: function() {
+
+        },
+
+        bind: function() {
+            
+        }
+    }
+
+    return {
+        init: function() {
+
+        }
+    }
+
+})()
